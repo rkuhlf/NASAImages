9eee983639e5164f5328254a7224d76f3e66ea06
Riley Kuhlman <rileyk42@icloud.com>
2018-04-01 07:25:56 -0500

    changed styles

--- js/main.js
+++ js/main.js
@@ -1,28 +1,23 @@
-
 var url = "https://api.nasa.gov/planetary/apod?api_key=Gb7nKSJOxm0wzDAesFQc0vTK7dJsiRwcmGqJ1kvQ";
 
 
 $.ajax({
-  url: url,
-  success: function(result){
-  if("copyright" in result) {
-    $("#copyright").text("Image Credits: " + result.copyright);
-  }
-  else {
-    $("#copyright").text("Image Credits: " + "Public Domain");
-  }
-  
-  if(result.media_type == "video") {
-    $("#apod_img_id").css("display", "none"); 
-    $("#apod_vid_id").attr("src", result.url);
-  }
-  else {
-    $("#apod_vid_id").css("display", "none"); 
-    $("#apod_img_id").attr("src", result.url);
-  }
-  $("#reqObject").text(url);
-  $("#returnObject").text(JSON.stringify(result, null, 4));  
-  $("#apod_explaination").text(result.explanation);
-  $("#apod_title").text(result.title);
-}
+    url: url,
+    success: function(result) {
+        if ("copyright" in result) {
+            $("#copyright").text("Image Credits: " + result.copyright);
+        } else {
+            $("#copyright").text("Image Credits: " + "Public Domain");
+        }
+
+        if (result.media_type == "video") {
+            $("#apodImgId").css("display", "none");
+            $("#apodVidId").attr("src", result.url);
+        } else {
+            $("#apodVidId").css("display", "none");
+            $("#apodImgId").attr("src", result.url);
+        }
+        $("#apodExplanation").text(result.explanation);
+        $("#apodTitle").text(result.title);
+    }
 });
